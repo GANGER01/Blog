007fffa8bf18b7a2900664e59ad3ae5c1a8c6e9b
GANGER01 <akshaygangernie211@gmail.com>
2022-06-15 22:11:35 +0530

    added components

---
+++ src/Components/Greet.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDom from "react";
+
+// function fun = () => <h1>Hello</h1>
+function Greet(){
+    return(
+        <h1>It gives some output</h1>
+    )
+}
+
+export default fun
